0a11975d38cdd7fd6fbd203af4a9af86fd3f9420
Maciej Kurek <maciejk77@gmail.com>
2018-08-01 22:50:36 +0100

    some helper methods, syntactic sugar, not sure if helpful

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ export function number2words(number) {
   function get_hundreds(n) {
     const word_for_hundreds = digits[Math.floor(n / 100)]
   
-    if(n % 100 === n) return get_tens(n)
-    if(n % 100 === 0) return word_for_hundreds + ' hundred'
+    if( is_lower_than_denominator(n, 100) ) return get_tens(n)
+    if( is_multiply_of(n, 100) ) return word_for_hundreds + ' hundred'
     if(n % 100 < 10) return word_for_hundreds + ' hundred ' + get_digit(n % 100)
     return word_for_hundreds + ' hundred ' + get_tens(n % 100)
   }
@@ -32,15 +32,11 @@ export function number2words(number) {
     if(is_teen(n)) { 
       return get_teens(n);
     } else {
-      return (n % 10 === 0) ? word_for_tens : word_for_tens + ' ' + get_digit(n % 10);
+      return ( is_multiply_of(n, 10) ) ? word_for_tens : word_for_tens + ' ' + get_digit(n % 10);
     }
 
-    if(n % 10 === n) return get_digit(n)
-    if(n % 10 === 0) return word_for_tens
-  }
-
-  function is_teen(n) {
-    return (n > 10 && n < 20) ? true : false
+    if( is_lower_than_denominator(n, 100) ) return get_digit(n)
+    if( is_multiply_of(n, 10) ) return word_for_tens
   }
 
   function get_teens(n) {
@@ -51,4 +47,17 @@ export function number2words(number) {
     return digits[n]
   }
 
+  // ===================================================
+  
+  function is_teen(n) {
+    return (n > 10 && n < 20) ? true : false
+  }
+
+  function is_multiply_of(numerator, denominator) { 
+    return (numerator % denominator === 0) ? true : false
+  }
+
+  function is_lower_than_denominator(numerator, denominator) { 
+    return (numerator % denominator === numerator) ? true : false
+  }
 }
